2d9fa1834c8b6ec7ede973b73495bfe563ac6e72
Vera_Ramiro <veram0227@gmail.com>
2025-07-12 16:38:35 -0300

    ME ENCANTÓ MI CALCULADORA, QUE MOTIVADOR

--- Giteo.bat
+++ Giteo.bat
@@ -5,7 +5,7 @@ echo Iniciando subida a GitHub...
 
 git init
 git add .
-git commit -m "Mejora CONTINUA, PERO MUY COSTOSAMENTE"
+git commit -m "ME ENCANTÓ MI CALCULADORA, QUE MOTIVADOR"
 git branch -M main
 rem git remote add origin https://github.com/matvera0608/CalculadoraSimple
 git push -u origin main
