dbc2d8b734ea004da93f09f2eff954fdc0aa6187
Vera_Ramiro <veram0227@gmail.com>
2025-09-10 15:54:12 -0300

    Subida del contenido actualizado.

--- calc_divisas.py
+++ calc_divisas.py
@@ -78,11 +78,12 @@ def cajas_de_texto(ventana):
      from calculadora_principal import formatearEntrada
      global entry_monto, origen, destino, conversión_variable, etiquetaDestino, etiquetaOrigen, color_padre
      color_padre = ventana.cget('bg')
+     
     
      etiquetaOrigen = tk.Label(ventana, bg=color_padre)
-     etiquetaOrigen.pack()
+     etiquetaOrigen.place(relx=0.1, rely=1.0, x=50, y=-200, anchor="se")
      etiquetaDestino = tk.Label(ventana, bg=color_padre)
-     etiquetaDestino.pack()
+     etiquetaDestino.place(relx=0.1, rely=1.0, x=50, y=-225, anchor="se")
 
      entry_monto = tk.Entry(ventana, font=("Courier New", 20), bd=4, justify="left")
      entry_monto.config(state="normal")
@@ -92,6 +93,9 @@ def cajas_de_texto(ventana):
      tk.Label(ventana, text="Monto a ingresar", font=("Courier New", 20), bg=color_padre).pack()
      
      tk.Label(ventana, text="Convertir de", font=("Courier New", 20), bg=color_padre).pack()
+     frm_Origen = tk.Frame(ventana, bg=color_padre)
+     frm_Origen.pack(pady=10)
+     
      origen = ttk.Combobox(ventana, values = list(divisas.keys()), font=("Courier New", 20), state="readonly")
      origen.set(list(divisas.keys())[0])
      origen.pack(pady=20)
@@ -100,6 +104,9 @@ def cajas_de_texto(ventana):
 
      #Tasa a ingresar
      tk.Label(ventana, text="a", font=("Courier New", 20), bg=color_padre).pack()
+     frm_Destino = tk.Frame(ventana, bg=color_padre)
+     frm_Destino.pack(pady=10)
+     
      destino = ttk.Combobox(ventana, values = list(divisas.keys()), font=("Courier New", 20), state="readonly")
      destino.set(list(divisas.keys())[1])
      destino.pack()
@@ -109,7 +116,7 @@ def cajas_de_texto(ventana):
      frmInvertir = tk.Frame(ventana, bg=color_padre)
      frmInvertir.place(relx=1.0, rely=1.0, x=-50, y=-225, anchor="se")
 
-     tk.Button(ventana, image=ventana.invertir, bg=color_padre, commmand=invertir_divisas).pack(padx=60)
+     tk.Button(ventana, image=ventana.invertir, bg=color_padre, command=invertir_divisas).pack(padx=60)
      #Resultado esperado
      conversión_variable = tk.StringVar()
      tk.Label(ventana, textvariable=conversión_variable, font=("Courier New", 20, "bold"), bg=color_padre, fg=color["verde_oscuro"]).pack()
